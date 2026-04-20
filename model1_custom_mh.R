6d40b764bcda7afe48681c9ad0015322c78d727b
Victor Liu <111852500+victor6754@users.noreply.github.com>
2026-04-19 17:59:39 -0700

    clean up model 1 custom mh, store posterior results

--- model1_custom_mh.R
+++ model1_custom_mh.R
@@ -1,5 +1,7 @@
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(require(cmdstanr))
+suppressPackageStartupMessages(require(mcmcse))
+suppressPackageStartupMessages(require(posterior))
 
 source("model1_custom_mh_functions.R")
 
@@ -7,14 +9,9 @@ source("model1_custom_mh_functions.R")
 # load in the crash_weather data
 # if not already done, run data.R first 
 # which will generate data/crash_weather.csv
-df <- read.csv("data/crash_weather.csv")
-
-
-set.seed(12345)
-
-df_simple <- df |>
-  slice_sample(prop = 0.1)
+df_simple <- read.csv("data/crash_weather.csv")
   
+
 
 set.seed(1234)
 
@@ -23,20 +20,16 @@ initial_beta_i <- rnorm(1, mean = 0, sd = 0.5)
 initial_beta_w <- rnorm(1, mean = 0, sd = 0.5)
 
 
-n <- 2000
+n <- 5000
 
 samples <- custom_mh(y = df_simple$Pedestrian.Flag, 
-                     intersection = df_simple$Intersection.Crash,
+                     intersection = df_simple$centered_intersection,
                      weekend = df_simple$weekend,
                      n_iters = n,
                      beta_0 = initial_beta_0,
                      beta_i = initial_beta_i,
                      beta_w = initial_beta_w)
 
-
-plot(samples$beta_0_trace)
-plot(samples$beta_i_trace)
-plot(samples$beta_w_trace)
 
 print(samples$acceptance_beta_0)
 print(samples$acceptance_beta_i)
@@ -48,11 +41,34 @@ beta_0_part_trace <- samples$beta_0_trace[1000:n]
 beta_i_part_trace <- samples$beta_i_trace[1000:n]
 beta_w_part_trace <- samples$beta_w_trace[1000:n]
 
-plot(beta_0_part_trace, type="l")
-plot(beta_i_part_trace, type="l")
-plot(beta_w_part_trace, type="l")
+pdf("figs/model1_custom_mh_trace_plots.pdf")
 
+plot(y=beta_0_part_trace, x=1:length(beta_0_part_trace), type="l", col="steelblue",
+     main="Trace Plot for beta_0", xlab="MCMC iteration", ylab="beta_0")
+plot(y=beta_i_part_trace, x=1:length(beta_i_part_trace), type="l", col="steelblue",
+     main="Trace Plot for beta_i", xlab="MCMC iteration", ylab="beta_i")
+plot(y=beta_w_part_trace, x=1:length(beta_w_part_trace), type="l", col="steelblue",
+     main="Trace Plot for beta_w", xlab="MCMC iteration", ylab="beta_w")
 
-hist(beta_0_part_trace)
-hist(beta_i_part_trace)
-hist(beta_w_part_trace)
+dev.off()
+
+pdf("figs/model1_custom_mh_histograms.pdf")
+
+hist(beta_0_part_trace, main="Posterior histogram for beta_0", 
+     xlab="beta_0", col="steelblue", border="white")
+hist(beta_i_part_trace, main="Posterior histogram for beta_i", 
+     xlab="beta_i", col="steelblue", border="white")
+hist(beta_w_part_trace, main="Posterior histogram for beta_w", 
+     xlab="beta_w", col="steelblue", border="white")
+
+dev.off()
+
+mcmc_matrix <- cbind(beta_0_part_trace, beta_i_part_trace, beta_w_part_trace)
+
+colnames(mcmc_matrix) <- c("beta_0", "beta_i", "beta_w")
+
+draws_obj <- as_draws_matrix(mcmc_matrix)
+fit_summary <- summarise_draws(draws_obj)
+print(fit_summary)
+
+write.csv(fit_summary, "results/model1_custom_mh_summary.csv", row.names=F)
